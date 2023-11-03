3819bd42ef979e6b4c460ea47d32520ba5bf5411
PraiseBidoof <147177385+HugoGallegoValle@users.noreply.github.com>
2023-11-03 18:05:55 +0100

    Clase 03112023

---
+++ Clase 03112023.py
@@ -0,0 +1,12 @@
+# Escribe tu código aquí :-)
+'''
+alumnos = []
+for cantidad in range(5):
+    alumnos.append(input("¿Cuál es el nombre del alumno?"))
+print(alumnos)
+'''
+#programa mio
+total = 0
+for cantidad in range(101):
+    total= cantidad + total
+print("El total es de",total)
--- Clase 27102023.py
+++ Clase 27102023.py
@@ -1,11 +1,11 @@
 # Escribe tu código aquí :-)
-'''
+
 alumnos = []
 tam = len(alumnos)
 while len(alumnos)<5:
     alumnos.append(input("¿Cuál es l nombre del alumno?"))
 print(alumnos)
-'''
+
 #pogrma mio (anterior
 numsecret = int(input("Elige un número"))
 print("Escribe un número entre 0 y 15")
@@ -19,3 +19,27 @@ while estado:
         print("El número que el oredenador esta pensando es menor")
     else:
         print("El número que el ordenador esta pensando es mayor")
+
+#programa de manera más corta
+numero = int(input("Elige un número: "))
+respuesta = int(input("¿Ahora tienes que intentar acertar el número: "))
+while respuesta != numero:
+    if respuesta > numero:
+        print("Tu numero es mayor del numero correcto")#
+    elif respuesta < numero:
+        print("Tu numero es menor que el número correcto")
+    respuesta = int(input("Ahora tienes que intentar acertar el número"))
+print("El número es el correcto")
+
+#otra manera de hacerlo
+numero = int(input("Elige un número"))
+fin = False
+while not fin:
+    respuesta = int(input("Elige un numero"))
+    if respuesta > numero:
+        print("Tu numero es mayor del numero correcto")#
+    elif respuesta < numero:
+        print("Tu numero es menor que el número correcto")
+    else:
+        fin = True
+print("El número es el correcto")

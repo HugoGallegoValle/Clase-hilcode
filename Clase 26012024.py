70d8b48c1f285a8e3290c87b9ffdd4e84988acba
Miguluf11 <147177385+HugoGallegoValle@users.noreply.github.com>
2024-01-26 18:16:07 +0100

    Update Clase 26012024.py

--- Clase 26012024.py
+++ Clase 26012024.py
@@ -1,4 +1,5 @@
 # Escribe tu código aquí :-)
+'''
 calculo_resto = 0
 calculo = 0
 variable = 0
@@ -17,14 +18,32 @@ print("|       Hola",nombre,"             |")
 print("|                              |")
 print("|                              |")
 print(" -----------------------------")
+'''
 #autoático centralizado
-calculo_resto = len(nombre)%25
-if calculo_resto == 0:
-    calculo = len(nombre)/25
-    print("|")
-    for espacios in range(0,calculo):
-        print(" ")
-    print("Holas",nombre)
-    for espacios in range(0,calculo):
-        print(" ")
-    print("|")
+espacios = " "
+espaciosrest = " "
+resto = 0
+calculo = 0
+nombre = input("¿Cómo te llamas?")
+calculo = 0
+resto = (30-len(nombre)-4)%2
+if calculo == 0:
+    calculo = (30-len(nombre)-4)/2
+        espacios = " " * calculo
+    print(" -----------------------------")
+    print("|                              |")
+    print("|                              |")
+    print("|",espacios"Hola",nombreespacios"|")
+    print("|                              |")
+    print("|                              |")
+    print(" -----------------------------")
+else:
+    calculo = (30-len(nombre)-4)/2
+    espaciosrest = claculo * " "
+    print(" -----------------------------")
+    print("|                              |")
+    print("|                              |")
+    print("|",espaciosrest,espacios,"Hola",nombre,espacios,"|")
+    print("|                              |")
+    print("|                              |")
+    print(" -----------------------------")

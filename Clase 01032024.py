ec8c947674442a4aa53eff5c913f9e83b1becf73
HugoGallegoValle <147177385+HugoGallegoValle@users.noreply.github.com>
2024-03-02 09:55:30 +0100

    Update Clase 01032024.py

--- Clase 01032024.py
+++ Clase 01032024.py
@@ -1,4 +1,5 @@
 # Escribe tu código aquí :-)
+# Escribe tu código aquí :-)
 def dibuja_tabla():
     tabla = """
      |       |
@@ -37,17 +38,36 @@ print(tabla)
 opcion = 1
 jugador1, jugador2 = pregunta()
 def elecciones(jugador1,jugador2):
-    lista = ["@"," "," "," "," "," "," "," "," "," "," "]
+    l = ["@"," "," "," "," "," "," "," "," "," "]
     opcion = 1
-    while " " in lista:
+    while " " in l:
         if opcion == 1:
             elección1 = int(input("Escribe la coordenada jugador 1"))
-            lista[elección1]=jugador1
+            while not l[elección1] == " ":
+                print("Escribe un valor valido")
+                elección1 = int(input("Escribe la coordenada jugador 1"))
+            l[elección1]=jugador1
             opcion = 2
-        if opcion == 2:
+        elif opcion == 2:
             elección2 = int(input("Escribe la coordenada jugador 2"))
-            lista[elección2]=jugador2
+            while not l[elección2] == " ":
+                print("Escribe un valor valido")
+                elección2 = int(input("Escribe la coordenada jugador 2"))
+            l[elección2]=jugador2
             opcion = 1
-    return lista
-lista = elecciones(jugador1,jugador2)
+            tabla = """
+       |       |
+   l[7]|l[8]   |l[9]
+       |       |
+  --------------------
+       |       |
+   l[4]|l[5]   |l[6]
+       |       |
+  --------------------
+       |       |
+   l[1]|l[2]   |l[3]
+       |       |
+"""
+    return l
+l = elecciones(jugador1,jugador2)
 print(lista)

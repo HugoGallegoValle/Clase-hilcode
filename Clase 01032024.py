353ebe8bcef484ee10436129f17648063e5f3d20
HugoGallegoValle <147177385+HugoGallegoValle@users.noreply.github.com>
2024-03-08 20:25:22 +0100

    Update Clase 01032024.py

--- Clase 01032024.py
+++ Clase 01032024.py
@@ -1,5 +1,4 @@
 # Escribe tu código aquí :-)
-# Escribe tu código aquí :-)
 def dibuja_tabla():
     tabla = """
      |       |
@@ -42,32 +41,31 @@ def elecciones(jugador1,jugador2):
     opcion = 1
     while " " in l:
         if opcion == 1:
-            elección1 = int(input("Escribe la coordenada jugador 1"))
-            while not l[elección1] == " ":
+            elección1 = int(input("Escribe la coordenada jugador 1:"))
+            while not (type(elección1) == int) or elección1 > 9 or elección1 < 1 or (l[elección1] != " "):
                 print("Escribe un valor valido")
-                elección1 = int(input("Escribe la coordenada jugador 1"))
+                elección1 = int(input("Escribe la coordenada jugador 1:"))
             l[elección1]=jugador1
             opcion = 2
         elif opcion == 2:
-            elección2 = int(input("Escribe la coordenada jugador 2"))
-            while not l[elección2] == " ":
+            elección2 = int(input("Escribe la coordenada jugador 2:"))
+            while not (type(elección2) == int) or elección2 > 9 or elección2 < 1 or (l[elección2] != " "):
                 print("Escribe un valor valido")
-                elección2 = int(input("Escribe la coordenada jugador 2"))
+                elección2 = int(input("Escribe la coordenada jugador 2:"))
             l[elección2]=jugador2
             opcion = 1
-            tabla = """
-       |       |
-   l[7]|l[8]   |l[9]
-       |       |
-  --------------------
-       |       |
-   l[4]|l[5]   |l[6]
-       |       |
-  --------------------
-       |       |
-   l[1]|l[2]   |l[3]
-       |       |
+        tabla = f"""
+          |       |
+      {l[9]}   |   {l[8]}   |   {l[7]}
+          |       |
+     --------------------
+          |       |
+      {l[6]}   |   {l[5]}   |   {l[4]}
+          |       |
+      --------------------
+          |       |
+      {l[3]}   |   {l[2]}   |   {l[1]}
+          |       |
 """
-    return l
-l = elecciones(jugador1,jugador2)
-print(lista)
+        print(tabla)
+elecciones(jugador1,jugador2)

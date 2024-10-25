1668d75dd599fe4f17fb504f80cd5f57948720e8
HYC <147177385+HugoGallegoValle@users.noreply.github.com>
2024-10-25 15:54:12 +0200

    Clase 24102024

---
+++ reto1.py
@@ -0,0 +1,27 @@
+# Escribe tu código aquí :-)
+lenguages=["python","c++","scratch","java","html"]
+for i in range(0,4):
+    print(f"Estoy aprendiendo {lenguages[i]}")
+'''
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print (lista[5:9])
+lista.append(60)       # Agrega al final
+print(lista)
+
+lista.extend([70, 80]) # Agrega múltiples elementos
+print(lista)
+
+lista.insert(2, 25)    # Inserta en el índice 2
+print(lista)
+
+lista.remove(25)
+print(lista)
+
+print(lista.pop()+5)
+listita=["b","a","c"]
+listita.sort()
+print(listita)
+listita.reverse()
+print(listita)
+print(count(b))
+print(count(a))

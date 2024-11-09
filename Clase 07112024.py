f2c30d0a1b56106131b2efabf73b259208f09e63
HugoGallegoValle <147177385+HugoGallegoValle@users.noreply.github.com>
2024-11-09 21:57:26 +0100

    Clase 07112024

---
+++ Clase 07112024.py
@@ -0,0 +1,13 @@
+# Escribe tu código aquí :-)
+diccionario = {
+    "dog":"guau",
+    "cat":"miau",
+    "cow":"muu"
+        }
+def canción(aniales,sonidos):
+    print(f'''Old MacDonald had a farm, E-I-E-I-O
+And on that farm he had a {animales}, E-I-E-I-O
+With a {sonidos}-{sonidos} here and a {sonidos}-{sonidos} there
+Here a {sonidos}, there a {sonidos}, everywhere a {sonidos}-{sonidos}''')
+for animales,sonidos in diccionario.items():
+    canción(animales,sonidos)

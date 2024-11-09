db3eec231ccb5d31df57fd30d6c3483a2c7de3c4
HugoGallegoValle <147177385+HugoGallegoValle@users.noreply.github.com>
2024-11-09 21:56:50 +0100

    Clase 07112024

---
+++ Clase 07112024 leer texto.py
@@ -0,0 +1,7 @@
+# Escribe tu código aquí :-)
+texto ="Hola soy Hugo 😀"
+def leer_texto(texto):
+    longitud = len(texto)
+    return longitud
+longitud = leer_texto(texto)
+print(f"El texto tiene: {longitud} caracteres")

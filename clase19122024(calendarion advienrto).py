16a7159d4e1e8a670fb5cd46eae49c723b092e3f
HYC <147177385+HugoGallegoValle@users.noreply.github.com>
2024-12-19 20:32:58 +0100

    Add files via upload

---
+++ clase19122024(calendarion advienrto).py
@@ -0,0 +1,32 @@
+# Escribe tu código aquí :-)
+with open("calendario_de_adviento","r+") as caledndario:
+    calendario.write='''
+    día1:
+    día2:
+    día3:
+    día4:
+    día5:
+    día6:
+    día7:
+    día8:
+    día9:
+    día10:
+    día11:
+    día12:
+    día13:
+    día14:
+    día15:
+    día16:
+    día17:
+    día18:
+    día19:
+    día20:
+    día21:
+    día22:
+    día23:
+    día24:
+    '''
+    '''
+    día=int(input("¿Qué día quieres actualizar?"))
+    actvidad=str(input(f"Qué actividad quieres realizar el día{día}"))
+    '''
